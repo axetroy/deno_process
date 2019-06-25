395a6a7bbca1ff5a1c8cfc93235d7f33c8d91b19
axetroy <troy450409405@gmail.com>
2019-06-25 23:32:46 +0800

    update

--- test.ts
+++ test.ts
@@ -1,11 +1,19 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { runIfMain, test } from "https://deno.land/std/testing/mod.ts";
+import {
+  assert,
+  assertEquals
+} from "https://deno.land/std@v0.9.0/testing/asserts.ts";
+import { runIfMain, test } from "https://deno.land/std@v0.9.0/testing/mod.ts";
+import { getProcess, getProcesses } from "./mod.ts";
 
-test({
-  name: "[math] sum",
-  fn(): void {
-    assertEquals(true, true);
-  }
+test(async function testGetProcess() {
+  const ps = await getProcess(Deno.pid);
+  assertEquals(ps.pid, Deno.pid);
+  assertEquals(ps.command, "deno");
+});
+
+test(async function testGetProcesses() {
+  const processList = await getProcesses();
+  assert(processList.length > 0);
 });
 
 runIfMain(import.meta);
