b5d453f7c397d0298f0be0edcc624933e4a652ab
axetroy <troy450409405@gmail.com>
2019-06-27 11:04:16 +0800

    update

--- test.ts
+++ test.ts
@@ -3,7 +3,25 @@ import {
   assertEquals
 } from "https://deno.land/std@v0.9.0/testing/asserts.ts";
 import { runIfMain, test } from "https://deno.land/std@v0.9.0/testing/mod.ts";
+import { join } from "https://deno.land/std@v0.9.0/fs/path/mod.ts";
 import { getProcess, getProcesses, killProcess } from "./mod.ts";
+
+export function homedir(): string {
+  let env = "HOME";
+  let envErr = "$HOME";
+
+  if (Deno.platform.os === "win") {
+    env = "USERPROFILE";
+    envErr = "%USERPROFILE%";
+  }
+
+  const value = Deno.env()[env];
+  if (value !== "") {
+    return value;
+  }
+
+  throw new Error(`Environment variable '${envErr}' is not defined.`);
+}
 
 function sleep(ms: number) {
   return new Promise((resolve, reject) => {
@@ -29,7 +47,12 @@ test(async function testGetProcesses() {
 test(async function testKillProcess() {
   const ps = run({
     args: [
-      "deno",
+      join(
+        homedir(),
+        ".deno",
+        "bin",
+        "deno" + (Deno.platform.os === "win" ? ".exe" : "")
+      ),
       "-A",
       "https://deno.land/std@v0.9.0/prettier/main.ts",
       "mod.ts"
